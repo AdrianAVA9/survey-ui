8357ae3dfbea93fb647fe4337673864f2df55590
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 12:39:44 -0600

    Adding create-survey page

--- survey-app/src/router/index.js
+++ survey-app/src/router/index.js
@@ -1,19 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'CreateSurvey',
+    component: () => import(/* webpackChunkName: "create-survey" */ '../views/CreateSurvey.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/create-survey',
+    name: 'CreateSurvey',
+    component: () => import(/* webpackChunkName: "create-survey" */ '../views/CreateSurvey.vue')
   }
 ]
 
---
+++ survey-app/src/views/CreateSurvey.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <SurveyForm/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SurveyForm from '@/components/SurveyForm.vue'
+
+export default {
+  name: 'CreateSurvey',
+  components: {
+    SurveyForm
+  }
+}
+</script>
