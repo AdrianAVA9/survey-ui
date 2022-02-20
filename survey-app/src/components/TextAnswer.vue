b6c51f4bf64b1226e3f41846dc90ceca01fc8866
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 15:24:32 -0600

    Layout text answer type

---
+++ survey-app/src/components/TextAnswer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <label>Ingrese su respuesta</label>
+    <textarea class="text-field" cols="3" rows="2"></textarea>
+  </div>
+</template>
+<script>
+export default {
+  name: "TextAnswer",
+};
+</script>
