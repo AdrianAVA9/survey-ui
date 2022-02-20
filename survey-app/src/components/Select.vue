1904885c19b35dc689d94e6d8d4c7184b5457cfd
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 14:29:55 -0600

    Layout select component

---
+++ survey-app/src/components/Select.vue
@@ -0,0 +1,15 @@
+<template>
+  <select class="text-field">
+    <option v-for="option in options" :key="option.id" :value="option.value">
+      {{ option.description }}
+    </option>
+  </select>
+</template>
+<script>
+export default {
+  name: "Select",
+  props: {
+    options: Array,
+  },
+};
+</script>
