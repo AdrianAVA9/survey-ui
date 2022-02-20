ac037e351a40e5e460b4fe220d40d8f134eaee1e
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 13:11:40 -0600

    Layout input field

---
+++ survey-app/src/components/Input.vue
@@ -0,0 +1,13 @@
+<template>
+  <input :type="type" class="text-field" :value="value" :placeholder="placeholder" />
+</template>
+<script>
+export default {
+  name: "Input",
+  prop: {
+    type: String,
+    placeholder: String,
+    value: String,
+  },
+};
+</script>
