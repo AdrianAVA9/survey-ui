059a0c9a287813cca2b7d7dd7f895236c0b6c2d7
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 22:15:05 -0600

    Adding events to option component

--- survey-app/src/components/OptionAnswer.vue
+++ survey-app/src/components/OptionAnswer.vue
@@ -2,47 +2,87 @@
   <div>
     <label class="answers__label">Opciones</label>
     <div class="answers">
-      <Answer
-        class=""
+      <Answer v-slot="scope"
         v-for="answer in answers"
         :key="answer.id"
         v-bind:answer="answer"
-      />
+        v-bind:canRemove="canRemove"
+        v-on:update-answer="updateAnswer"
+        v-on:delete-answer="deleteAnswer"
+      >
+        <Input
+          class="text-field__sm"
+          :value="answer.value"
+          :placeholder="'Digite una respuesta'"
+          @input="scope.change"
+        />
+      </Answer>
     </div>
-    <Button class="btn--primary-outline btn-ex-sm fw-600" v-bind:text="'+ Agregar'" />
+    <Button
+      class="btn--primary-outline btn-ex-sm fw-600"
+      v-bind:text="'+ Agregar'"
+      @click="addAnswer"
+    />
   </div>
 </template>
 <script>
 import Answer from "@/components/Answer";
 import UniqueId from "@/Utils/UniqueId.js";
 import Button from "@/components/Button";
+import Input from "@/components/Input";
 
 export default {
   name: "OptionAnswer",
   components: {
     Answer,
     Button,
+    Input,
+  },
+  beforeMount() {
+    for (let index = 1; index <= 3; index++) this.addAnswer();
   },
   data() {
     return {
-      answers: [
-        {
-          id: UniqueId.generate(),
-          number: 1,
-          value: "",
-        },
-        {
-          id: UniqueId.generate(),
-          number: 2,
-          value: "",
-        },
-        {
-          id: UniqueId.generate(),
-          number: 3,
-          value: "",
-        },
-      ],
+      canRemove: true,
+      counter: 1,
+      answers: [],
     };
+  },
+  methods: {
+    updateAnswer: function (answer) {
+      let existingAnswer = this.answers.find((a) => a.id === answer.id);
+      if (!existingAnswer) return;
+
+      existingAnswer.value = answer.value;
+      this.emitUpdate();
+    },
+    deleteAnswer: function (answer) {
+      this.answers = this.answers.filter((a) => a.id !== answer.id);
+      this.resetAnswerQuestion();
+      this.emitUpdate();
+    },
+    addAnswer: function () {
+      this.answers.push({
+        id: UniqueId.generate(),
+        number: this.counter,
+        value: "",
+      });
+
+      this.counter++;
+    },
+    resetAnswerQuestion() {
+      this.counter = 1;
+      this.answers.forEach((answer) => {
+        answer.number = this.counter;
+        this.counter++;
+      });
+    },
+    emitUpdate: function () {
+      this.$emit(
+        "update-answers",
+        this.answers.filter((a) => a.value.length > 0)
+      );
+    },
   },
 };
 </script>
