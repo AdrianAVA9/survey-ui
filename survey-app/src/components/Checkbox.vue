25373842d58a584040c37a8c63bce1ef8a2cb193
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 23:45:24 -0600

    Layout checkbox

---
+++ survey-app/src/components/Checkbox.vue
@@ -0,0 +1,21 @@
+<template>
+  <label :for="checkboxId">
+    <input type="checkbox" :name="name" :id="checkboxId" />
+    {{ description }}
+  </label>
+</template>
+<script>
+import UniqueId from "@/Utils/UniqueId.js";
+export default {
+  name: "Checkbox",
+  props: {
+    description: String,
+    name: String,
+  },
+  data() {
+    return {
+      checkboxId: UniqueId.generate(),
+    };
+  },
+};
+</script>
