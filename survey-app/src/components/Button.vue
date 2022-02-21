2fbf2ae7baec21652c3d8174491064d78255b895
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 19:34:31 -0600

    Layout button component

---
+++ survey-app/src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="btn" :disabled="disabled" @click="() => this.$emit('click')">
+    <span
+      v-if="state === 'loading'"
+      class="spinner-border spinner-border__sm"
+      role="status"
+      aria-hidden="true"
+    ></span>
+    <span v-if="state !== 'loading'" v-html="text"></span>
+  </button>
+</template>
+<script>
+export default {
+  name: "Button",
+  props: {
+    state: String,
+    text: String,
+    disabled: Boolean,
+  },
+};
+</script>
