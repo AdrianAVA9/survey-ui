9de6a34a8137d73ca54524c3d617c97aea236743
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 22:13:31 -0600

    Adding events and pasing custom components

--- survey-app/src/components/Answer.vue
+++ survey-app/src/components/Answer.vue
@@ -1,42 +1,64 @@
 <template>
   <div class="answer">
     <span class="answer__number">{{ answer.number }}</span>
-    <Input class="text-field__sm" :value="answer.value" :placeholder="'Digite una respuesta'" />
-    <Button class="btn--default btn-sm" v-bind:text="icon" />
+    <slot :change="editAnswer"></slot>
+    <Button
+      v-if="canRemove"
+      class="btn--default btn-sm"
+      v-bind:text="icon"
+      @click="deleteAnswer"
+    />
   </div>
 </template>
 <script>
-import Input from "@/components/Input";
+//import Input from "@/components/Input";
 import Button from "@/components/Button";
 
 export default {
   name: "Answer",
   components: {
-    Input,
+    /* Input, */
     Button,
   },
   props: {
     answer: Object,
+    canRemove: Boolean,
   },
   data() {
     return {
       icon: '<i class="fas fa-times"></i>',
     };
   },
+  methods: {
+    deleteAnswer: function () {
+      this.$emit("delete-answer", {
+        id: this.answer.id,
+      });
+    },
+    editAnswer: function (evt) {
+      this.$emit("update-answer", {
+        id: this.answer.id,
+        value: evt.target.value,
+      });
+    },
+  },
 };
 </script>
 <style scoped>
-.answer{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+.answer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
-.answer > *:not(:last-child){
-    margin-right: var(--gap-2);
+.answer > *:last-child {
+  margin-left: var(--gap-2);
 }
-.answer__number{
-    width: 10px;
-    font-size: .825rem;
-    font-weight: 600;
+.answer > *:first-child {
+  margin-right: var(--gap-2);
+}
+.answer__number {
+  width: 10px;
+  font-size: 0.825rem;
+  font-weight: 600;
 }
 </style>
