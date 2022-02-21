2d53735e93d47b60845ef3254d0d396064cb4c14
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 23:45:05 -0600

    Layout survey-form and question preview

--- survey-app/src/views/CreateSurvey.vue
+++ survey-app/src/views/CreateSurvey.vue
@@ -1,17 +1,43 @@
 <template>
-  <div>
-    <SurveyForm/>
+  <div class="form__layout">
+    <SurveyForm v-on:add-question="addNewQuestion" />
+    <SurveyPreview v-bind:questions="questions" v-on:delete-question="deleteQuestion" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import SurveyForm from '@/components/SurveyForm.vue'
+import SurveyForm from "@/components/SurveyForm.vue";
+import SurveyPreview from "@/components/SurveyPreview.vue";
+import UniqueId from "@/Utils/UniqueId.js";
 
 export default {
-  name: 'CreateSurvey',
+  name: "CreateSurvey",
   components: {
-    SurveyForm
-  }
-}
+    SurveyForm,
+    SurveyPreview,
+  },
+  data() {
+    return {
+      questions: [],
+    };
+  },
+  methods: {
+    addNewQuestion: function (question) {
+      let newQuestion = Object.assign({}, question);
+      newQuestion.id = UniqueId.generate();
+      this.questions.push(newQuestion);
+    },
+    deleteQuestion: function (question) {
+      this.questions = this.questions.filter((q) => q.id !== question.id);
+    },
+  },
+};
 </script>
+
+<style scoped>
+.form__layout {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: var(--gap-1);
+}
+</style>
