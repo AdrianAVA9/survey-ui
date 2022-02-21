f40766a1d69894ac6ba16b232b3b47d4920371e5
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-20 23:45:52 -0600

    Layout option-question preview

---
+++ survey-app/src/components/OptionQuestionPreview.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="answers">
+    <Checkbox
+      v-for="answer in answers"
+      :key="answer.id"
+      v-bind:description="answer.value"
+      v-bind:name="answerName"
+    />
+  </div>
+</template>
+<script>
+import Checkbox from "@/components/Checkbox";
+import UniqueId from "@/Utils/UniqueId.js";
+export default {
+  name: "OptionQuestionPreview",
+  components: {
+    Checkbox,
+  },
+  props: {
+    answers: Array,
+  },
+  data() {
+    return {
+      answerName: UniqueId.generate(),
+    };
+  },
+};
+</script>
+<style scoped>
+.answers > * {
+  display: block;
+  padding-bottom: var(--gap-1);
+}
+</style>
